ae1e39964d4a997ecc04637659897d6724ced752
Tommy Sullivan <tom@cleveweb.com>
2014-11-01 02:20:02 +0000

    conjugating elaa and soutaa

--- presentTenseConjugator.js
+++ presentTenseConjugator.js
@@ -2,12 +2,20 @@ module.exports = function() {
     return {
         conjugate: function(infinitive,pronoun) {
             pronoun = pronoun.toLowerCase();
-            if(pronoun=='i') return 'elän';
-            else if(pronoun=='you') return 'elät';
-            else if(pronoun=='we') return 'elämme';
-            else if(pronoun=="y'all") return 'elätte';
-            else if(pronoun=='they') return 'elävät';
-            else return 'elää'; //if(pronoun=='he' || pronoun=='she' || pronoun=='it') 
+            function conjugateStem(strongStem, weakStem, pronoun) {
+                var aToUse = strongStem.indexOf('ä')!=-1 ? 'ä' : 'a';
+                if(pronoun=='minä') return weakStem + 'n';
+                else if(pronoun=='sinä') return weakStem + 't';
+                else if(pronoun=='me') return weakStem + 'mme';
+                else if(pronoun=='te') return weakStem + 'tte';
+                else if(pronoun=='he') return strongStem + 'v'+aToUse+'t';
+                else return strongStem + aToUse;
+            }
+            if(infinitive=='elää') {
+                return conjugateStem('elä', 'elä', pronoun);
+            } else {
+                return conjugateStem('souta', 'souda', pronoun);
+            }
         }
     }
 }
