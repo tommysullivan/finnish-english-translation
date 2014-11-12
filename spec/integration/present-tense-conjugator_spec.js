5902801c3984b2ab95eb6859af4f512f11d9e58e
Tommy Sullivan <tom@cleveweb.com>
2014-11-12 22:17:28 +0000

    added present tense positive and negative test cases for review by my sweetie

--- spec/integration/present-tense-conjugator_spec.js
+++ spec/integration/present-tense-conjugator_spec.js
@@ -1,23 +1,25 @@
 var FinnishEnglishTranslation = require('../../finnishEnglishTranslation');
 var finnishEnglishTranslator = FinnishEnglishTranslation();
 var presentTenseConjugator = finnishEnglishTranslator.createPresentTenseConjugator();
+var perfectTenseConjugator = finnishEnglishTranslator.createPerfectTenseConjugator();
+
+function expectConjugation(infinitive, pronoun, expected, methodToCall) {
+    describe('and pronoun is '+pronoun, function() {
+        it('should be '+expected, function() {
+            expect(methodToCall(infinitive, pronoun)).toBe(expected);
+        }); 
+    }); 
+}
+function expectInfinitiveConjugations(infinitive, arrayOfExpectedConjugations, shouldTestNegation) {
+    describe('when infinitive is '+infinitive, function() {
+        function expectConjugationForPronoun(pronoun, index) {
+            expectConjugation(infinitive, pronoun, arrayOfExpectedConjugations[index], shouldTestNegation);
+        }
+        ['minä', 'sinä', 'han / se', 'me', 'te', 'he'].forEach(expectConjugationForPronoun);
+    });
+}
 
 describe('PresentTenseConjugator', function() {
-    function expectConjugation(infinitive, pronoun, expected, methodToCall) {
-        describe('and pronoun is '+pronoun, function() {
-            it('should be '+expected, function() {
-                expect(methodToCall(infinitive, pronoun)).toBe(expected);
-            }); 
-        }); 
-    }
-    function expectInfinitiveConjugations(infinitive, arrayOfExpectedConjugations, shouldTestNegation) {
-        describe('when infinitive is '+infinitive, function() {
-            function expectConjugationForPronoun(pronoun, index) {
-                expectConjugation(infinitive, pronoun, arrayOfExpectedConjugations[index], shouldTestNegation);
-            }
-            ['minä', 'sinä', 'han / se', 'me', 'te', 'he'].forEach(expectConjugationForPronoun);
-        });
-    }
     describe('conjugate(infinitive, person)', function() {
         //TODO: Maybe we should test different capitalizations!
         function expectPositiveConjugations(infinitive, arrayOfExpectedConjugations) {
@@ -44,5 +46,35 @@ describe('PresentTenseConjugator', function() {
         expectNegativeConjugations('nähdä', ['en näe', 'et näe', 'ei näe', 'emme näe', 'ette näe', 'eivät näe']);
         expectNegativeConjugations('syödä', ['en syö', 'et syö', 'ei syö', 'emme syö', 'ette syö', 'eivät syö']);
         expectNegativeConjugations('olla', ['en ole', 'et ole', 'ei ole', 'emme ole', 'ette ole', 'eivät ole']);
+    });
+});
+
+describe('PerfectTenseConjugator', function() {
+    describe('conjugate(infinitive, person)', function() {
+        //TODO: Maybe we should test different capitalizations!
+        function expectPositiveConjugations(infinitive, arrayOfExpectedConjugations) {
+            expectInfinitiveConjugations(infinitive, arrayOfExpectedConjugations, perfectTenseConjugator.conjugate);    
+        }
+        expectPositiveConjugations('elää', ['olen elänyt', 'olet elänyt', 'on elänyt', 'olemme eläneet', 'olette eläneet', 'ovat eläneet']);
+        expectPositiveConjugations('soutaa', ['olen soudanut', 'olet soudanut', 'on soudanut', 'olemme soudaneet', 'olette soudaneet', 'ovat soudaneet']);
+        expectPositiveConjugations('takellella', ['olen takeltellut', 'olet takeltellut', 'on takeltellut', 'olemme takeltelleet', 'olette takeltelleet', 'ovat takeltelleet']);
+        expectPositiveConjugations('kokea', ['olen koenut', 'olet koenut', 'on koenut', 'olemme koeneet', 'olette koeneet', 'ovat koeneet']);
+        expectPositiveConjugations('voida', ['olen voinut', 'olet voinut', 'on voinut', 'olemme voineet', 'olette voineet', 'ovat voineet']);
+        expectPositiveConjugations('nähdä', ['olen näenyt', 'olet näenyt', 'on näenyt', 'olemme näeneet', 'olette näeneet', 'ovat näeneet']);
+        expectPositiveConjugations('syödä', ['olen syönyt', 'olet syönyt', 'on syönyt', 'olemme syöneet', 'olette syöneet', 'ovat syöneet']);
+        expectPositiveConjugations('olla', ['olen ollut', 'olet ollut', 'on ollut', 'olemme olleet', 'olette olleet', 'ovat olleet']);
+    });
+    describe('conjugateNegation(infinitive, person)', function() {
+        function expectNegativeConjugations(infinitive, arrayOfExpectedConjugations) {
+            expectInfinitiveConjugations(infinitive, arrayOfExpectedConjugations, perfectTenseConjugator.conjugateNegation);    
+        }
+        expectNegativeConjugations('elää', ['en ollut elänyt', 'et ollut elänyt', 'ei ollut elänyt', 'emme olleet eläneet', 'ette olleet eläneet', 'eivät olleet eläneet']);
+        expectNegativeConjugations('soutaa', ['en ollut soudanut', 'et ollut soudanut', 'ei ollut soudanut', 'emme olleet soudaneet', 'ette olleet soudaneet', 'eivät olleet soudaneet']);
+        expectNegativeConjugations('takellella', ['en ollut takeltellut', 'et ollut takeltellut', 'ei ollut takeltellut', 'emme olleet takeltelleet', 'olette takeltelleet', 'eivät olleet takeltelleet']);
+        expectNegativeConjugations('kokea', ['en ollut koenut', 'et ollut koenut', 'ei ollut koenut', 'emme olleet koeneet', 'ette olleet koeneet', 'eivät olleet koeneet']);
+        expectNegativeConjugations('voida', ['en ollut voinut', 'et ollut voinut', 'ei ollut voinut', 'emme olleet voineet', 'ette olleet voineet', 'eivät olleet voineet']);
+        expectNegativeConjugations('nähdä', ['en ollut näenyt', 'et ollut näenyt', 'ei ollut näenyt', 'emme olleet näeneet', 'ette olleet näeneet', 'eivät olleet näeneet']);
+        expectNegativeConjugations('syödä', ['en ollut syönyt', 'et ollut syönyt', 'ei ollut syönyt', 'emme olleet syöneet', 'ette olleet syöneet', 'eivät olleet syöneet']);
+        expectNegativeConjugations('olla', ['en ollut ollut', 'et ollut ollut', 'ei ollut ollut', 'emme olleet olleet', 'ette olleet olleet', 'eivät olleet olleet']);
     });
 });
