d679d6e6cc33ecdb3747aa95287d22ab2381cec2
Thomas Sullivan <thomassullivan@Thomass-MacBook-Pro.local>
2019-08-17 13:30:33 +0300

    remove jasmine

--- spec/integration/pluralizer_spec.ts
+++ spec/integration/pluralizer_spec.ts
@@ -1,29 +1,28 @@
 import FinnishEnglishTranslation from '../../FinnishEnglishTranslation'
 import expect from "expect"
-
 import { stemPluralPairs } from "./test-data/stem-plural-pairs"
-const pluralizer = FinnishEnglishTranslation().createPluralizer()
 
-describe('Pluralizer', function() {
-    describe('pluralize for stem', function() {
+const pluralizer = () => FinnishEnglishTranslation().createPluralizer()
+
+describe('Pluralizer', () => {
+    describe('pluralize for stem', () => {
         stemPluralPairs.forEach(function(stemPluralPair) {
-            const stem = stemPluralPair[0]
-            const expectedPlural = stemPluralPair[1]
-            describe(stem, function() {
-                it('gives '+expectedPlural, function() {
-                    expect(pluralizer.pluralize(stem)).toEqual(expectedPlural)
+            const [ stem, expectedPlural ] = stemPluralPair
+            describe(stem, () => {
+                it('gives '+expectedPlural, () => {
+                    expect(pluralizer().pluralize(stem)).toEqual(expectedPlural)
                 }) 
             })   
         })
     })
-    describe('complexity', function() {
-        it('should be less than 25', function() {
-            expect(pluralizer.complexity()).toBeLessThan(300) 
+    describe('complexity', () => {
+        it('should be less than 25', () => {
+            expect(pluralizer().complexity()).toBeLessThan(300) 
         }) 
     })
-    describe('number of rules', function() {
-        it('should be less than 10', function() {
-            expect(pluralizer.numTopLevelRules()).toBeLessThan(50)
+    describe('number of rules', () => {
+        it('should be less than 10', () => {
+            expect(pluralizer().numTopLevelRules()).toBeLessThan(50)
         }) 
     })
 })
